cad15fd752b8c416c19d77987c8dfd1d1f505c04
gartunius <gabriel.ataidesantos@gmail.com>
2021-04-25 20:13:55 -0300

    Busca em largura funcionando, a ser checada, testado apenas em matriz 3x3

--- LighsOutAI/Gerente.py
+++ LighsOutAI/Gerente.py
@@ -16,13 +16,11 @@ class Gerente:
         self.criar_estado_raiz()
         self.estados_abertos.append(self.estado_raiz)
 
-        try:
-            while True:
-                for estado in self.estados_abertos:
-                    self._adicionar_aos_estados_abertos(self.criar_tabuleiro(estado))
-
-        except KeyboardInterrupt:
-            print(f"Encerrando\nestados abertos: {self.estados_abertos}\nestados fechados: {self.estados_fechados}")
+        while True:
+            estado = self.estados_abertos[0]
+            if self._checar_estado(estado):
+                break
+            self._adicionar_aos_estados_abertos(self.criar_tabuleiro(estado))
 
     def _adicionar_aos_estados_abertos(self, estados):
         for estado in estados:
@@ -42,20 +40,22 @@ class Gerente:
         for linha in range(estado.tamanhoTabuleiro):
             for coluna in range(estado.tamanhoTabuleiro):
                 novo_estado = copy.deepcopy(estado)
-                novo_estado.alternar_valor_do_quadrado(linha, coluna)
-                # self._return_pretty_pecas(novo_estado.pecas)
-                estados_filhos.append(novo_estado)
+                checagem_modicicacao = novo_estado.alternar_valor_do_quadrado(linha, coluna)
+                if checagem_modicicacao is not None and novo_estado not in self.estados_fechados:
+                    estados_filhos.append(novo_estado)
 
         return estados_filhos
 
-    def _return_pretty_pecas(self, pecas):
-        for linha in pecas:
-            print(linha)
-
-    def _checar_tabuleiro(self, tabuleiro: Tabuleiro):
-        for linha in tabuleiro.pecas:
+    def _checar_estado(self, estado: Tabuleiro):
+        print(estado)
+        for linha in estado.pecas:
             for coluna in linha:
                 if coluna != 0:
+                    if estado in self.estados_abertos:
+                        self.estados_abertos.remove(estado)
+                        self.estados_fechados.append(estado)
+                    else:
+                        raise Exception("o estado não esta na lista dos estados abertos")
                     return False
 
         return True
--- LighsOutAI/Tabuleiro.py
+++ LighsOutAI/Tabuleiro.py
@@ -4,17 +4,24 @@ from LighsOutAI.Posicao import Posicao
 class Tabuleiro:
     pecas = [[]]
     tamanhoTabuleiro: int
-    # ultimoClique = Posicao
+    # TODO: converter de lista para objeto 'Posicao'
     # linha,coluna
     ultimaMudanca = [None, None]
 
-    def __init__ (self, pecas, tamanho_tabuleiro: int):
+
+    def __init__(self, pecas, tamanho_tabuleiro: int):
         self.pecas = pecas
         self.tamanhoTabuleiro = tamanho_tabuleiro
+
+    def __str__(self):
+        return str(self.pecas)
 
     def alternar_valor_do_quadrado(self, linha: int, coluna: int):
         if linha == self.ultimaMudanca[0] and coluna == self.ultimaMudanca[1]:
             return None
+
+        self.ultimaMudanca[0] = linha
+        self.ultimaMudanca[1] = coluna
 
         self._mudar_valor(linha, coluna)
         try:
@@ -30,56 +37,28 @@ class Tabuleiro:
         if coluna >= 1:
             self._mudar_valor(linha, coluna - 1)
 
+        return not None
+
     def _mudar_valor(self, linha, coluna):
         if self.pecas[linha][coluna] == 0:
             self.pecas[linha][coluna] = 1
         elif self.pecas[linha][coluna] == 1:
             self.pecas[linha][coluna] = 0
 
-    @property
     def getPecas(self):
         return self._pecas
 
-    # @pecas.setter
     def setPecas(self, posicao, valor):
         posicao = Posicao
         self.pecas[posicao.getLinha][posicao.getColuna] = valor
 
-    @property
     def getTamanhoTab(self):
         return self._tamanhoTab
 
-    @property
     def getUltimoClique(self):
         return self._ultimoClique
 
-    # @ultimoClique.setter
     def setUltimoClique(self, novoClique):
         novoClique = Posicao
         self.ultimoClique.setPosicao(novoClique.getLinha(), novoClique.getColuna())       
 
-    def fazerAcao(self):
-        auxiliar = self.getUltimoClique()
-        novaCol = auxiliar.setColuna(auxiliar.getColuna+1)
-        novaLinha = auxiliar.setLinah(auxiliar.setLinha+1)
-        novaPos = Posicao
-
-        if auxiliar.getLinha == self.tamanhoTab-1 and auxiliar.getColuna != self.tamanhoTab-1:
-            novaPos.setColuna(novaCol)
-        else:
-            novaPos.setLinha(novaLinha)
-
-        if auxiliar.getColuna+1 == 0 or auxiliar.getColuna > self.tamanhoTab:
-            self.setPecas()
-
-#tabuleiro = Tabuleiro()
-#
-#def inverteValores(valor):
-#    if valor == 0:
-#        valor = 1
-#        return valor
-#    elif valor == 1:
-#        valor = 0
-#        return valor
-#    else:
-#      return valor
